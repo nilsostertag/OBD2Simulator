645a51e8e1053b500d037cfee77c27eb1db3ea40
nilsostertag <nostertag@gmx.de>
2023-05-11 16:59:38 +0200

    added few randomized measurements to be output in hex

--- OBD2Simulator/main.cpp
+++ OBD2Simulator/main.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
-#include <cstdlib> // for rand() and srand()
-#include <ctime>   // for time()
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
 #include <cmath>
+#include <sstream>
 
-int randomInteger(int min, int max) {
+int RandomInteger(int min, int max) {
     srand(time(0));
     return min + (rand() % (max - min + 1));
 }
 
-float randomFloat(float min, float max) {
+float RandomFloat(float min, float max) {
     srand(time(0));
     return min + round(static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (max - min))) * 10) / 10.0;
 }
 
+//Still needs to be corrected
+template<typename T>
+std::string ToHex(T value) {
+    char buffer[sizeof(T) * 2 + 3];
+    sprintf_s(buffer, "0x%0*X", (int)(sizeof(T) * 2), (unsigned)value);
+    return std::string(buffer);
+}
+
 int main() {
-    std::cout << randomInteger(0, 100) << std::endl;
-    std::cout << randomFloat(0.0, 100) << std::endl;
+    std::string velovity = ToHex(RandomInteger(0, 255));
+    std::string acceleration = ToHex(RandomFloat(0, 20));
+    std::string enginerpm = ToHex(RandomInteger(0, 7500));
+    std::string engineload = ToHex(RandomInteger(0, 100));
+    std::string throttlepos = ToHex(RandomInteger(0, 100));
+    std::string fuelatm = ToHex(RandomFloat(0, 25));
+    std::string fuelavg = ToHex(RandomFloat(0, 40));
+
+    std::cout << velovity + "\n" + acceleration + "\n" + enginerpm + "\n" + engineload + "\n" + throttlepos + "\n" + fuelatm + "\n" + fuelavg << std::endl;
+
     return 0;
 }
